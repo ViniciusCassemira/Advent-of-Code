9cd7e05b68d483121284142c99748d9c4c356a98
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-07-07 15:43:48 -0300

    feat: add part2 solution

--- 2015/day3.py
+++ 2015/day3.py
@@ -33,4 +33,36 @@ print(positions)
 
 uniquePosition = set(positions)
 numUniquePosition = len(uniquePosition)
-print(f'Result: {numUniquePosition}')
+print(f'Result part1: {numUniquePosition}')
+
+# ----------------  PART 2  ----------------
+startPosition2 = (0, 0)
+positions2 = set()
+currentPosition2 = startPosition2
+currentPositionRobot2 = startPosition2
+
+# Initial gift
+positions2.add(startPosition2)
+positions2.add(currentPositionRobot2)
+
+def newGift(p, current):
+    currentPosition = (current[0] + p[0], current[1] + p[1])
+    positions2.add(currentPosition)
+    return currentPosition
+
+for i, arrow in enumerate(input):
+    if arrow == ">":
+        pos = (0, 1)
+    elif arrow == "<":
+        pos = (0, -1)
+    elif arrow == "^":
+        pos = (1, 0)
+    elif arrow == "v":
+        pos = (-1, 0)
+
+    if i % 2 == 0:
+        currentPosition2 = newGift(pos, currentPosition2)
+    else:
+        currentPositionRobot2 = newGift(pos, currentPositionRobot2)
+
+print(f'Result part 2: {len(positions2)}')
