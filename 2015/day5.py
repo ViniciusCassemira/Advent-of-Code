9bc066005d01a4af792c5aa14e862280ba551119
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-11-02 07:17:05 -0300

    ✨ feat: day5 part II

--- 2015/day5.py
+++ 2015/day5.py
@@ -48,3 +48,44 @@ for string in input:
 print(niceStrings)  
 
 # ------ part 2 -------
+
+# Functions
+
+def compairArray(array):
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i]["data"] == array[j]["data"]:
+                if abs(array[i]["x"] - array[j]["x"]) >= 2:
+                    return True
+    return False
+
+def betweenString(string):
+    for i in range(len(string)-2):
+        if string[i] == string[i+2]:
+            return True
+        
+def doublePair(string):
+    datas = []
+    for i in range(len(string)-1):
+    #agrupar por dupla e pegar as coordenadas de cada letra
+        pair = string[i]+string[i+1]
+        x = i
+        y = i + 1
+
+        data = {
+            "data": pair,
+            "x": x,
+            "y": y
+        }
+        datas.append(data)
+    return compairArray(datas)
+
+def validateString2(string):
+    return betweenString(string) and doublePair(string)
+
+niceStrings2 = 0
+
+for string in input:
+    if validateString2(string):
+        niceStrings2 += 1
+print(niceStrings2)  
